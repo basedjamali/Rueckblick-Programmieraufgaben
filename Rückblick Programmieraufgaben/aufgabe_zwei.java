e0cd4e249300689bc9545cbbabc05fb09e273a7c
Based Jamali <basedjamali@gmail.com>
2024-09-04 16:16:04 +0200

    Add files via upload

---
+++ Rückblick Programmieraufgaben/aufgabe_zwei.java
@@ -0,0 +1,7 @@
+public class aufgabe_zwei {
+       public static void main(String[] args){
+              int x = InOut.readInt("Geben Sie eine Zahl ein: ");
+              int y = ((((x * 2) + 84) / 2) - x);
+              System.out.println("y beträgt: " + y);                    
+       }
+  }
